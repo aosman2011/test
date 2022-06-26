cf353ddcf99ffee1032a8b368ac94287e880fa56
aosman2011 <108238131+aosman2011@users.noreply.github.com>
2022-06-26 23:07:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("mohamed");
+var FavorouteDinosaurs = ["Diplodocus","Velocoraptor","Stegosaurus"]
+console.log(FavorouteDinosaurs);
+
+FavorouteDinosaurs.push("Lastasarus")
+
+FavorouteDinosaurs.reverse()
+FavorouteDinosaurs.push("Firstasaurus")
+FavorouteDinosaurs.reverse()
+console.log(FavorouteDinosaurs)
+
